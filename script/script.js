bbe70941cb5ece3df9ff4a18b1c6c172c14ac058
rnjsgusdk <20210705-slideshow03>
2021-07-05 14:47:31 +0900

    20210705-slideshow03

---
+++ script/script.js
@@ -0,0 +1,24 @@
+/*eslint-disable*/
+$(function(){
+    function(){
+        setInterval(fnSlide,800);
+
+        function fnSlide(){
+            $("slide a:first-chlid").fadeOut(300,
+        function(){
+            $("#slide a:first-chlid")
+            .insertAfter
+             $("#slide a:last-chlid")
+
+    });
+     $("slide a:nth-chlid(2)").fadeIn(500);
+        }
+
+    }
+//    1. 첫째를 feadout 한다
+//    1-1 첫째에 콜백 함수를 만드렁 순서를 마지막으로 이동 한다
+//
+//    2. 둘째를 feadin 한다
+
+
+});
